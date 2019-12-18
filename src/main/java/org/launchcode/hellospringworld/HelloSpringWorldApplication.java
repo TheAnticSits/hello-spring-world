e555cda9522bd04a0842ac848f087f008be51496
Ken Messmer <kenmessmerjr@gmail.com>
2019-12-18 16:51:21 -0600

    Hello and Goodbye controllers using ResponseBody and GetMapping with GetMapping("hello") and GetMapping("goodbye").

---
+++ src/main/java/org/launchcode/hellospringworld/HelloSpringWorldApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/hellospringworld/HelloSpringWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
