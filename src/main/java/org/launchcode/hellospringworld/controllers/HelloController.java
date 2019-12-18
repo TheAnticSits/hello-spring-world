1a5e77720952ccc11eec15154d11c709ac1672b3
Ken Messmer <kenmessmerjr@gmail.com>
2019-12-18 16:57:38 -0600

    Created Dynamic Request Handler

--- src/main/java/org/launchcode/hellospringworld/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospringworld/controllers/HelloController.java
@@ -3,23 +3,32 @@ package org.launchcode.hellospringworld.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     //Handles request at path /hello
-    @GetMapping("hello")
+/*    @GetMapping("hello")
     @ResponseBody
     public String hello(){
 
         return "Hello, Spring!";
-    }
+    }*/
 
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye(){
 
         return "Goodbye, Spring!";
+    }
+
+    // Handles request of form /hello?name=LaunchCode
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
