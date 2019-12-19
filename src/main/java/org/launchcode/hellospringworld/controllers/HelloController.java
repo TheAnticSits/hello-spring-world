9357ca66e51bace94dc3c43523a47f6473dff21e
Ken Messmer <kenmessmerjr@gmail.com>
2019-12-18 19:49:47 -0600

    uses data that is part of the path itself to return as variable

--- src/main/java/org/launchcode/hellospringworld/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospringworld/controllers/HelloController.java
@@ -3,6 +3,7 @@ package org.launchcode.hellospringworld.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -31,4 +32,13 @@ public class HelloController {
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
+
+    //handles requests of the form /hello/LaunchCode
+    //uses data that is part of the path itself to return as variable
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String hellowWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
 }
