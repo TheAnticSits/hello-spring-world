b2c375db3fb9b473079ac63fb9c7cc2902e445fe
Ken Messmer <kenmessmerjr@gmail.com>
2019-12-20 21:56:35 -0600

    changed helloAgain to model Model created hello-list.html used a for loop in the template created List ArrayList with names

--- src/main/java/org/launchcode/hellospringworld/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospringworld/controllers/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
@@ -18,9 +21,10 @@ public class HelloController {
     }
 
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("form")
@@ -28,4 +32,14 @@ public class HelloController {
         return "form";
     }
 
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("Becca");
+        names.add("Leela");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
 }
