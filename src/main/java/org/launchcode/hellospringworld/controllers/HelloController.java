16ab5c7dd5db481da541f0f3c1a5bf44d1df41ee
Ken Messmer <kenmessmerjr@gmail.com>
2019-12-20 19:58:38 -0600

    Cleaned up code

--- src/main/java/org/launchcode/hellospringworld/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospringworld/controllers/HelloController.java
@@ -14,32 +14,13 @@ public class HelloController {
 
         return "Hello, " + name + "!";
     }
-/*    // lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye(){
 
-        return "Goodbye, Spring!";
-    }
-
-    // Handles request of form /hello?name=LaunchCode
-    //this requestmapping is general version of postmapping.  Put them in annotation parameters.
-    //lives at /hello/hello
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }*/
-
-    //handles requests of the form /hello/LaunchCode
-    //uses data that is part of the path itself to return as variable
     @GetMapping("hello/{name}")
     @ResponseBody
     public String helloAgain(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
-    //form if not specified otherwise will be submitted via get request
-    // lives at hello/form
     @GetMapping("form")
     public String helloform(){
         return "form";
